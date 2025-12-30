0ac572498ff963888e344e3470b61556996913ec
arduinocodes11 <jalaltablet08@gmail.com>
2025-12-29 21:26:40 -0500

    Update Sequential_blinking.ino

--- Sequential_blinking.ino
+++ Sequential_blinking.ino
@@ -1,5 +1,5 @@
 
-/* A simple program to sequentially turn on and turn off 3 LEDs */ 
+
 
 int LED1 = 13;
 int LED2 = 12;
@@ -14,15 +14,15 @@ void setup() {
 
 void loop() {
   digitalWrite(LED1, HIGH);    // turn on LED1 
-  delay(50);                  // wait for 200ms
+  delay(50);                  // wait for 100ms
   digitalWrite(LED2, HIGH);    // turn on LED2
-  delay(50);                  // wait for 200ms       
+  delay(50);                  // wait for 100ms       
   digitalWrite(LED3, HIGH);    // turn on LED3 
-  delay(50);                  // wait for 200ms
+  delay(50);                  // wait for 100ms
   digitalWrite(LED1, LOW);     // turn off LED1
-  delay(50);                  // wait for 300ms
+  delay(50);                  // wait for 200ms
   digitalWrite(LED2, LOW);     // turn off LED2
-  delay(50);                  // wait for 300ms
+  delay(50);                  // wait for 200ms
   digitalWrite(LED3, LOW);     // turn off LED3
-  delay(50);                  // wait for 300ms before running program all over again
+  delay(50);                  // wait for 200ms before running program again
 }
